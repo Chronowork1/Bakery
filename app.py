d7e1814029fb346aa319e07cf10483bec22d6d40
Chronowork1 <jianxie6464@gmail.com>
2020-08-15 23:56:38 -0700

    Implement password confirmation

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_wtf import FlaskForm, Form
 from wtforms import StringField, PasswordField, BooleanField
-from wtforms.validators import InputRequired, Email, Length, email_validator
+from wtforms.validators import InputRequired, Email, Length, email_validator, EqualTo
 from flask_sqlalchemy  import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
@@ -26,7 +26,7 @@ class User(UserMixin, db.Model):
     id=db.Column(db.Integer, primary_key=True)
     username= db.Column(db.String(15), unique=True)
     email = db.Column(db.String(50), unique=True)
-    password= db.Column(db.String(80))
+    password = db.Column(db.String(80))
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -43,6 +43,7 @@ class RegisterForm(FlaskForm):
     email = StringField('email', validators=[InputRequired(), Email(message='Invalid email'), Length(max=50)])
     username = StringField('username', validators=[InputRequired(), Length(min=4, max=15)])
     password = PasswordField('password', validators=[InputRequired(), Length(min=8, max=80)])
+    confirm =  PasswordField('confirm', validators=[InputRequired(), EqualTo('password', message="Password must match")])
 
 #Route to template
 @app.route('/')
